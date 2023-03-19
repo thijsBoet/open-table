4b665cd56a7529c9f5f013a205612efd8c7a8593
thijsBoet <m.boet@chello.nl>
2023-03-19 12:46:56 +0100

    added title tags for pages and favicon.ico

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -7,7 +7,6 @@ import { RestaurantNavbar } from '../components/RestaurantNavbar';
 export default function RestaurantMenu() {
 	return (
 		<>
-			<Navbar />
 			<div className='bg-white w-[100%] rounded p-3 shadow'>
 				<RestaurantNavbar />
 				<Menu />
