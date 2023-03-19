9b65fe18e34f419fa7909689906c02d20613c088
thijsBoet <m.boet@chello.nl>
2023-03-19 11:23:41 +0100

    created layout tsx file

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -11,24 +11,22 @@ import Title from './components/Title';
 
 export default function RestaurantDetails() {
 	return (
-		<main className='bg-gray-100 min-h-screen w-screen'>
-			<main className='max-w-screen-2xl m-auto bg-white'>
-				<Navbar />
-				<Header />
-				<div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-					<div className='bg-white w-[70%] rounded p-3 shadow'>
-						<RestaurantNavbar />
-						<Title />
-						<Rating />
-						<Description />
-						<Images />
-						<Reviews />
-					</div>
-					<div className='w-[27%] relative text-reg'>
-						<ReservationCard />
-					</div>
+		<>
+			<Navbar />
+			<Header />
+			<div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
+				<div className='bg-white w-[70%] rounded p-3 shadow'>
+					<RestaurantNavbar />
+					<Title />
+					<Rating />
+					<Description />
+					<Images />
+					<Reviews />
 				</div>
-			</main>
-		</main>
+				<div className='w-[27%] relative text-reg'>
+					<ReservationCard />
+				</div>
+			</div>
+		</>
 	);
 }
